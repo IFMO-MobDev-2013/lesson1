6a80c03d279c639cc94335a7c4ae603ab796bdbd
Mikhail Ermakov <mikhail@mermakov.ru>
2013-09-12 21:46:34 +0400

    Add apk

---
+++ Whirl/gen/ru/mermakov/projects/Whirl/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package ru.mermakov.projects.Whirl;
+
+/* This stub is for using by IDE only. It is NOT the Manifest class actually packed into APK */
+public final class Manifest {
+}
