b46cd1a4315b8c5e35c8887ab4945f728a7590a8
SkripnikovSI <skripnikov.sergey@gmail.com>
2013-09-12 18:21:10 +0400

    Скрипников Сергей 2538

---
+++ androidhm1/src/ru/ifmo/ctddev/skripnikov/androidhm1/MyActivity.java
@@ -0,0 +1,159 @@
+package ru.ifmo.ctddev.skripnikov.androidhm1;
+
+import java.util.Calendar;
+import java.util.Random;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class MyActivity extends Activity {
+    int w = 240;
+    int h = 320;
+    Bitmap bmp = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+    int[] pixels = new int[w * h];
+    float wk = 3;
+    float hk = 3;
+    long time = 1;
+    int[][] field = new int[h + 2][w + 2];
+    Paint p = new Paint();
+
+    public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+        private DrawThread drawThread;
+
+        public MySurfaceView(Context context) {
+            super(context);
+            getHolder().addCallback(this);
+        }
+
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width,
+                                   int height) {
+        }
+
+        @Override
+        public void surfaceCreated(SurfaceHolder holder) {
+            wk = getWidth() / (float) 240.0;
+            hk = getHeight() / (float) 320.0;
+            drawThread = new DrawThread(getHolder());
+            drawThread.setRunning(true);
+            drawThread.start();
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+            boolean retry = true;
+            drawThread.setRunning(false);
+            while (retry) {
+                try {
+                    drawThread.join();
+                    retry = false;
+                } catch (InterruptedException e) {
+                }
+            }
+        }
+
+
+        class DrawThread extends Thread {
+            private boolean runFlag = false;
+            private SurfaceHolder surfaceHolder;
+
+            public DrawThread(SurfaceHolder surfaceHolder) {
+                this.surfaceHolder = surfaceHolder;
+
+            }
+
+            public void setRunning(boolean run) {
+                runFlag = run;
+            }
+
+            @Override
+            public void run() {
+                Canvas canvas;
+                while (runFlag) {
+                    canvas = null;
+                    try {
+                        canvas = surfaceHolder.lockCanvas(null);
+                        synchronized (surfaceHolder) {
+                            long newTime = Calendar.getInstance().getTimeInMillis();
+                            long d = newTime - time;
+                            time = newTime;
+                            long fps = 1000 / d;
+                            nextField();
+                            drawField(canvas);
+                            canvas.drawText(Long.toString(fps) + " FPS", 100, 100, p);
+                        }
+                    } finally {
+                        if (canvas != null) {
+                            surfaceHolder.unlockCanvasAndPost(canvas);
+                        }
+                    }
+                }
+            }
+
+            private void nextField() {
+                newBorder();
+                int[][] newField = new int[h + 2][w + 2];
+                for (int i = 1; i <= h; i++)
+                    for (int j = 1; j <= w; j++) {
+                        int next = (field[i][j] + 1) % 16;
+                        if (next == field[i - 1][j - 1]
+                                || next == field[i - 1][j]
+                                || next == field[i - 1][j + 1]
+                                || next == field[i][j - 1]
+                                || next == field[i][j + 1]
+                                || next == field[i + 1][j - 1]
+                                || next == field[i + 1][j]
+                                || next == field[i + 1][j + 1]) {
+                            newField[i][j] = next;
+                            pixels[(i - 1) * w + j - 1] = Color.argb(255, next * 15, 128, 255 - next * 15);
+                        } else {
+                            newField[i][j] = field[i][j];
+                        }
+                    }
+                field = newField;
+            }
+
+            private void drawField(Canvas canvas) {
+                bmp.setPixels(pixels, 0, w, 0, 0, w, h);
+                canvas.drawBitmap(Bitmap.createScaledBitmap(bmp, getWidth(), getHeight(), false), 10, 10, null);
+                //canvas.drawBitmap(pixels, 0, w, 0, 0, w, h, false, p);
+            }
+
+            private void newBorder() {
+                for (int i = 1; i <= w; i++) {
+                    field[0][i] = field[h][i];
+                    field[h + 1][i] = field[1][i];
+                }
+                for (int i = 1; i <= h; i++) {
+                    field[i][0] = field[i][w];
+                    field[i][w + 1] = field[i][1];
+                }
+                field[0][0] = field[h][w];
+                field[h + 1][0] = field[1][w];
+                field[h + 1][w + 1] = field[1][1];
+                field[0][w + 1] = field[h][1];
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Random r = new Random();
+        p.setTextSize(100);
+        p.setARGB(255, 255, 255, 255);
+        for (int i = 1; i <= h; i++)
+            for (int j = 1; j <= w; j++) {
+                field[i][j] = r.nextInt(16);
+                pixels[(i - 1) * w + j - 1] = Color.argb(255, 100 + field[i][j] * 5, field[i][j] * 100, 200 - field[i][j] * 5);
+            }
+        super.onCreate(savedInstanceState);
+        setContentView(new MySurfaceView(this));
+    }
+}
