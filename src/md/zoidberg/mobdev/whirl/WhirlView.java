d896c286a738b23e51f6c9a737bd258fcf983b77
Gregory Fefelov <gfv@zoidberg.md>
2013-09-12 22:06:10 +0400

    first commit

---
+++ src/md/zoidberg/mobdev/whirl/WhirlView.java
@@ -0,0 +1,11 @@
+package md.zoidberg.mobdev.whirl;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: gfv
+ * Date: 12.09.13
+ * Time: 22:03
+ * To change this template use File | Settings | File Templates.
+ */
+public class WhirlView {
+}
