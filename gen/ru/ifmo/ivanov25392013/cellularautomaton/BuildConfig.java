cefbc55149aa2dd3236dc55eadcbe2462068798f
unknown <wb0011@mail.ru>
2013-09-12 18:40:20 +0400

    Ivanov Ilya 2539

---
+++ gen/ru/ifmo/ivanov25392013/cellularautomaton/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+/** Automatically generated file. DO NOT MODIFY */
+package ru.ifmo.ivanov25392013.cellularautomaton;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
