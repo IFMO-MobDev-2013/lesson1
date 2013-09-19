4d468cc9482f1ec705b448da659373a86a4bde56
Kachaev Sergey <FireSpace666@gmail.com>
2013-09-19 19:30:41 +0400

    fix bugs

--- MainActivity.java
+++ MainActivity.java
@@ -10,13 +10,14 @@ import android.view.View;
 import java.util.Random;
 
 public class MainActivity extends Activity {
-    final int WIDTH = 240;
-    final int HEIGHT = 320;
-
-    int[] colors = new int[10];
-    int[][] screen = new int[HEIGHT][WIDTH];
+    public static final int WIDTH = 240;
+    public static final int HEIGHT = 320;
+    public static final int COUNT_COLOURS = 10;
+    public static final int[] colors =  {Color.BLACK, Color.BLUE, Color.CYAN, Color.DKGRAY, Color.GRAY, Color.GREEN, Color.LTGRAY, Color.MAGENTA, Color.RED, Color.YELLOW};
+    
+    int[][] tempscreen = new int[HEIGHT][WIDTH]; int [][] screen = new int[HEIGHT][WIDTH];
+    int[] forbitmap = new int[HEIGHT*WIDTH+1]; int ifb = 0; // iterator for forbitmap
     Bitmap bitmap;
-    Matrix matrix = new Matrix();
     Paint paint = new Paint();
     long frames = 0;
 
@@ -27,52 +28,42 @@ public class MainActivity extends Activity {
 
         @Override
         public void onDraw(Canvas canvas) {
-            int screenHeight = canvas.getHeight();
-            int screenWidth = canvas.getWidth();
-            canvas.scale((float)screenWidth / WIDTH, (float)screenHeight / HEIGHT);
-            canvas.drawBitmap(bitmap, matrix, paint);
+            ifb = 0;
 
             for (int i = 0; i < HEIGHT; i++) {
                 for (int j = 0; j < WIDTH; j++) {
-                    int tempcolour = screen[i][j] + 1; tempcolour = (tempcolour == 10) ? 0 : tempcolour;
+                    int tempcolour = screen[i][j] + 1; tempcolour = (tempcolour == COUNT_COLOURS) ? 0 : tempcolour;
                     
                     if (screen[(i-1 < 0) ? HEIGHT-1 : i-1][(j-1 < 0) ? WIDTH-1 : j-1] == tempcolour || screen[i][(j-1 < 0) ? WIDTH-1 : j-1] == tempcolour || screen[(i+1 >= HEIGHT) ? 0 : i+1][(j-1 < 0) ? WIDTH-1 : j-1] == tempcolour
                             || screen[(i-1 < 0) ? HEIGHT-1 : i-1][j] == tempcolour || screen[(i+1 >= HEIGHT) ? 0 : i+1][j] == tempcolour || screen[(i-1 < 0) ? HEIGHT-1 : i-1][(j+1 >= WIDTH) ? 0 : j+1] == tempcolour 
                             || screen[i][(j+1 >= WIDTH) ? 0 : j+1] == tempcolour || screen[(i+1 >= HEIGHT) ? 0 : i+1][(j+1 >= WIDTH) ? 0 : j+1] == tempcolour) {
-                        screen[i][j] = tempcolour;
-                        bitmap.setPixel(j, i, colors[tempcolour]);
+                        tempscreen[i][j] = tempcolour;
+                        forbitmap[ifb++] = colors[tempscreen[i][j]];
                     }
                 }
             }
 
-            canvas.drawText(Long.toString(1000 * (frames++) / SystemClock.currentThreadTimeMillis()), 10, 10, paint);
+            bitmap.setPixels(forbitmap, 1, WIDTH, 0, 0, WIDTH, HEIGHT);
+            canvas.scale((float)canvas.getWidth() / WIDTH, (float)canvas.getHeight() / HEIGHT);
+            canvas.drawBitmap(bitmap, 0, 0, paint);
+            canvas.drawText(Long.toString(1000 * (frames++) / SystemClock.currentThreadTimeMillis()), 20, 40, paint);
             invalidate();
+            for (int i = 0; i < HEIGHT; ++i) for (int j = 0; j < WIDTH; ++j) screen[i][j] = tempscreen[i][j];
         }
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        colors[0] = Color.BLACK;
-        colors[1] = Color.BLUE;
-        colors[2] = Color.CYAN;
-        colors[3] = Color.DKGRAY;
-        colors[4] = Color.GRAY;
-        colors[5] = Color.GREEN;
-        colors[6] = Color.LTGRAY;
-        colors[7] = Color.MAGENTA;
-        colors[8] = Color.RED;
-        colors[9] = Color.YELLOW;
-
-        bitmap = Bitmap.createBitmap(WIDTH, HEIGHT, Bitmap.Config.ARGB_4444);
+        bitmap = Bitmap.createBitmap(WIDTH, HEIGHT, Bitmap.Config.RGB_565);
         Random rand = new Random();
         for (int i = 0; i < HEIGHT; i++) {
             for (int j = 0; j < WIDTH; j++) {
-                screen[i][j] = rand.nextInt(10);
-                bitmap.setPixel(j, i, colors[screen[i][j]]);
+            	screen[i][j] = rand.nextInt(COUNT_COLOURS);
+            	forbitmap[ifb++] = colors[screen[i][j]];
             }
         }
 
-
+        bitmap.setPixels(forbitmap, 1, WIDTH, 0, 0, WIDTH, HEIGHT);
 
         super.onCreate(savedInstanceState);
         setContentView(new WhirlView(this));
